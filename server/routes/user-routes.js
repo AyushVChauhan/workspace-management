30b0d6c64eba70846029c90b6d9152189b2138a3
Ayush Chauhan <ayush2004chauhan2526@gmail.com>
2024-06-29 13:18:44 +0530

    amenity availability

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { authMiddleware } = require('../middlewares/auth-middleware');
 const { asyncRouteHandler } = require('../utils/router-utils');
 const { verify } = require('../controllers/common-controller');
-const { bookRoom, getAvailability } = require('../controllers/user-controller');
+const { bookRoom, getAvailability, getAmenityAvailability } = require('../controllers/user-controller');
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.use(authMiddleware('USER'));
 router.get('/verify', asyncRouteHandler(verify));
 
 router.post('/booking/availability/:roomId', asyncRouteHandler(getAvailability));
+router.post('/booking/availability/amenity/:roomId', asyncRouteHandler(getAmenityAvailability));
 router.post('/booking/book/:roomId', asyncRouteHandler(bookRoom));
 
 module.exports = router;
