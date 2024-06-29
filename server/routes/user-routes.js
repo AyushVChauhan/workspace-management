f8acc80a43d4b1a97e5106357fe674fca0c4bec4
Ayush Chauhan <ayush2004chauhan2526@gmail.com>
2024-06-29 13:20:16 +0530

    workspace routes user

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -3,11 +3,15 @@ const { authMiddleware } = require('../middlewares/auth-middleware');
 const { asyncRouteHandler } = require('../utils/router-utils');
 const { verify } = require('../controllers/common-controller');
 const { bookRoom, getAvailability, getAmenityAvailability } = require('../controllers/user-controller');
+const { getWorkspaces, getWorkspace } = require('../controllers/admin-controller');
 
 const router = express.Router();
 
 router.use(authMiddleware('USER'));
 router.get('/verify', asyncRouteHandler(verify));
+
+router.get('/workspace', asyncRouteHandler(getWorkspaces));
+router.get('/workspace/:id', asyncRouteHandler(getWorkspace));
 
 router.post('/booking/availability/:roomId', asyncRouteHandler(getAvailability));
 router.post('/booking/availability/amenity/:roomId', asyncRouteHandler(getAmenityAvailability));
